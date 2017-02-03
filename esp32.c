3a6b4208739aa306b0735a3cb4ce700255e175e2
Martin Falatic <martin@falatic.com>
2017-02-03 01:25:34 -0800

    Removed nudge; use esync in _getCycleCount FWIW; slightly less glitchy espShow() and test code

--- esp32.c
+++ esp32.c
@@ -6,24 +6,26 @@
 
 #include <Arduino.h>
 
+// esync from https://github.com/espressif/arduino-esp32/blob/master/cores/esp32/Esp.cpp
 static uint32_t _getCycleCount(void) __attribute__((always_inline));
 static inline uint32_t _getCycleCount(void) {
   uint32_t ccount;
-  __asm__ __volatile__("rsr %0,ccount":"=a" (ccount));
+  __asm__ __volatile__("esync; rsr %0,ccount":"=a" (ccount));
   return ccount;
 }
 
 void espShow(
- uint8_t pin, uint8_t *pixels, uint32_t numBytes, boolean is800KHz) {
+  uint8_t pin, uint8_t *pixels, uint32_t numBytes, boolean is800KHz) {
 
-#define TIMING_NUDGE 1.0
-
-#define CYCLES_800_T0H  (F_CPU / (int)(2500000 * TIMING_NUDGE)) // 0.4us
-#define CYCLES_800_T1H  (F_CPU / (int)(1250000 * TIMING_NUDGE)) // 0.8us
-#define CYCLES_800      (F_CPU / (int)( 800000 * TIMING_NUDGE)) // 1.25us per bit
-#define CYCLES_400_T0H  (F_CPU / (int)(2000000 * TIMING_NUDGE)) // 0.5uS
-#define CYCLES_400_T1H  (F_CPU / (int)( 833333 * TIMING_NUDGE)) // 1.2us
-#define CYCLES_400      (F_CPU / (int)( 400000 * TIMING_NUDGE)) // 2.5us per bit
+// 12.500ns/cycle @  80MHz
+//  4.167ns/cycle @ 240MHz
+#define CYCLES_RESET    (F_CPU /  142800) // 2500000 = 7.0us
+#define CYCLES_800_T0H  (F_CPU / 2500000) // 2500000 = 0.4us
+#define CYCLES_800_T1H  (F_CPU / 1250000) // 1250000 = 0.8us
+#define CYCLES_800      (F_CPU /  800000) //  800000 = 1.25us per bit
+#define CYCLES_400_T0H  (F_CPU / 2000000) // 2000000 = 0.5uS
+#define CYCLES_400_T1H  (F_CPU /  833333) //  833333 = 1.2us
+#define CYCLES_400      (F_CPU /  400000) //  400000 = 2.5us per bit
 
   uint8_t *p, *end, pix, mask;
   uint32_t t, time0, time1, period, c, startTime, pinMask;
@@ -49,20 +51,72 @@ void espShow(
   }
 #endif
 
-  for(t = time0;; t = time0) {
-    if(pix & mask) t = time1;                             // Bit high duration
-    while(((c = _getCycleCount()) - startTime) < period); // Wait for bit start
+#ifdef TRY1
+// Glitchy
+  for (t = time0;; t = time0) {
+    if (pix & mask) t = time1;                             // Bit high duration
+    while (((c = _getCycleCount()) - startTime) < period); // Wait for bit start
     gpio_set_level(pin, HIGH);
-    startTime = c;                                        // Save start time
-    while(((c = _getCycleCount()) - startTime) < t);      // Wait high duration
+    startTime = c;                                         // Save start time
+    while (((c = _getCycleCount()) - startTime) < t);      // Wait high duration
     gpio_set_level(pin, LOW);
-    if(!(mask >>= 1)) {                                   // Next bit/byte
-      if(p >= end) break;
+    if (!(mask >>= 1)) {                                   // Next bit/byte
+      if (p >= end) break;
       pix  = *p++;
       mask = 0x80;
     }
   }
   while((_getCycleCount() - startTime) < period); // Wait for last bit
+#endif
+
+#define TRY2
+#ifdef TRY2
+// Derived from
+// https://kbeckmann.github.io/2015/07/25/Bit-banging-two-pins-simultaneously-on-ESP8266/
+// glitchy only on first pass thru loop. Weird.
+  startTime = _getCycleCount() - period;
+  while (true) {
+    while (((c = _getCycleCount()) - startTime) < period); // Wait for bit start
+    gpio_set_level(pin, HIGH);
+    startTime = c;                                         // Save start time
+    t = time0;
+    if (pix & mask) t = time1;                             // Bit high duration
+    while (((c = _getCycleCount()) - startTime) < t);      // Wait high duration
+    gpio_set_level(pin, LOW);
+    if (!(mask >>= 1)) {                                   // Next bit/byte
+      if (p >= end) break;
+      pix  = *p++;
+      mask = 0x80;
+    }
+  }
+  while (((c = _getCycleCount()) - startTime) < period); // Wait for last bit
+#endif
+
+//#define TRY3
+#ifdef TRY3
+// Derived from
+// https://github.com/Makuna/NeoPixelBus/blob/master/src/internal/NeoPixelEsp8266.c
+// https://kbeckmann.github.io/2015/07/25/Bit-banging-two-pins-simultaneously-on-ESP8266/
+// https://wp.josh.com/2014/05/13/ws2812-neopixels-are-not-so-finicky-once-you-get-to-know-them/
+  startTime = _getCycleCount();
+  while (((c = _getCycleCount()) - startTime) < CYCLES_RESET*200); // VERY glitchy!
+  startTime = _getCycleCount() - period;
+  while (true) {
+    while (((c = _getCycleCount()) - startTime) < period); // Wait for bit start
+    gpio_set_level(pin, HIGH);
+    startTime = c;                                         // Save start time
+    t = time0;
+    if (pix & mask) t = time1;                             // Bit high duration
+    while (((c = _getCycleCount()) - startTime) < t);      // Wait high duration
+    gpio_set_level(pin, LOW);
+    if (!(mask >>= 1)) {                                   // Next bit/byte
+      if (p >= end) break;
+      pix  = *p++;
+      mask = 0x80;
+    }
+  }
+  while (((c = _getCycleCount()) - startTime) < period); // Wait for last bit
+#endif
 }
 
 #endif // ESP32
